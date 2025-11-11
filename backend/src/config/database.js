3d4a4bd42b6766057edf409d8d3903a1ac9b56b8
JV-FIU <jvald.fiu@hotmail.com>
2025-11-11 11:00:10 -0500

    Created Database Table Creation Files
    Added the files and code necessary to create the database's tables.
    
    Also modified package.json to include the "reset" command.

---
+++ backend/src/config/database.js
@@ -0,0 +1,21 @@
+// By: Jorge Valdes-Santiago
+//
+//
+// This script retrieves the environment variables from the
+// local .env file and sets up the resource pool to
+// allow the server to communicate with the database.
+import pg from "pg";
+import "./dotenv.js";
+
+const config = {
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+export const pool = new pg.Pool(config);
---
+++ backend/src/config/dotenv.js
@@ -0,0 +1,4 @@
+// Needed to load the .env environment variables
+// to process.env
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
