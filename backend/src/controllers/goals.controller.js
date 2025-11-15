d41d9186f621d3d6dd358619a29f74d9e7338d0a
JV-FIU <jvald.fiu@hotmail.com>
2025-11-15 00:02:25 -0500

    Created goals CRUD
    Created the backend code necessary for managing the goals table

--- backend/src/controllers/goals.controller.js
+++ backend/src/controllers/goals.controller.js
@@ -1,52 +1,36 @@
 // Created by: Jorge Valdes-Santiago
 //
 //
-// This script contains the functions for perfoming CRUD operations on the "tasks" table
+// This script contains the functions for perfoming CRUD operations on the "goals" table
 const { pool } = require("../config/database.js"); //import { pool } from "../config/database.js";
 
-const createTask = async (req, res) => {
-  // Send query to the database
+const createGoal = async (req, res) => {
   try {
     // Get parameters from HTTP POST request
-    const {
-      user_id,
-      goal_id,
-      title,
-      description,
-      is_urgent,
-
-      is_important,
-      target_date,
-      est_time_minutes,
-      status,
-    } = req.body;
+    const { user_id, title, description, status, target_date } = req.body;
 
     // Get current time
     const created_at = new Date.now().toISOString(); // Get date in UTC (this can be converted to the local timezone in the frontend)
 
-    // Query to add a new task to the tasks table
+    // Query to add a new goal to the goals table
     const query = `
-    INSERT INTO tasks (user_id, goal_id, title, description, is_urgent, is_important, target_date, est_time_minutes, status, created_at)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    INSERT INTO goals (user_id, title, description, status, target_date, created_at)
+    VALUES ($1, $2, $3, $4, $5, $6)
     `;
 
+    // Send query to database
     const results = await pool.query(query, [
       user_id,
-      goal_id,
       title,
       description,
-      is_urgent,
-
-      is_important,
-      target_date,
-      est_time_minutes,
       status,
+      target_date,
       created_at,
     ]);
 
     // Request successful
     res.status(201).json(results.rows[0]);
-    console.log("New task created");
+    console.log("New goal created");
   } catch (error) {
     // Request not successful
     res.status(409).json({ error: error.message });
@@ -54,113 +38,98 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTaskById = async (req, res) => {
+const getGoalById = async (req, res) => {
   try {
-    const query = `SELECT * FROM tasks WHERE id = $1`;
+    const query = `SELECT * FROM goals WHERE id = $1`;
     const id = parseInt(req.params.id); // Get item id (IMPORTANT: THE PARAMETER MUST MATCH THE ONE USED IN THE routes file)
 
     const results = await pool.query(query, [id]);
 
-    res.status(200).json(results.rows[0]); // Request successful, send message to client
+    res.status(200).json(results.rows[0]); // Request successful, send data to client
   } catch (error) {
     // Request not successful
-    res.status(409).json({ error: err.message });
+    res.status(409).json({ error: error.message });
     console.log("Error:", error.message);
   }
 };
 
-const getTasksByUserId = async (req, res) => {
+// Gets goals by user id
+const getGoalsByUserId = async (req, res) => {
   try {
-    // Get all tasks from a user id
+    // Get all goals from a user id
     const { user_id } = req.body; // Get user_id from the request body
 
-    const query = `SELECT * FROM tasks WHERE user_id = $1`;
+    const query = `SELECT * FROM goals WHERE user_id = $1`;
 
     const results = await pool.query(query, [user_id]); // Send query to the database
 
     res.status(200).json(results.rows); // Request successful, send data to client
   } catch (error) {
     // Request not successful
-    res.status(409).json({ error: err.message });
+    res.status(409).json({ error: error.message });
     console.log("Error:", error.message);
   }
 };
 
-const updateTask = async (req, res) => {
+const updateGoal = async (req, res) => {
   try {
     const id = parseInt(req.params.id); //  parse the ID from the URL parameter and convert it to an integer.
 
     // Get parameters from HTTP PATCH request
-    const {
-      user_id,
-      goal_id,
-      title,
-      description,
-      is_urgent,
-
-      is_important,
-      target_date,
-      est_time_minutes,
-      status,
-    } = req.body;
+    const { user_id, title, description, status, target_date } = req.body;
 
     const query = `
-    UPDATE tasks SET title = $1, description = $2, is_urgent = $3, 
-    is_important = $4, target_date = $5, est_time_minutes = $6, 
-    status = $7 WHERE id = $8`;
+    UPDATE goals SET title = $1, description = $2, status = $3, 
+    target_date = $5 WHERE id = $6`;
     const results = await pool.query(query, [
       title,
       description,
-      is_urgent,
-
-      is_important,
-      target_date,
-      est_time_minutes,
       status,
+      target_date,
       id,
     ]);
 
     res.status(200).json(results.rows); // Request successful, send response to client
-    console.log("Task updated");
+    console.log("Goal updated");
   } catch (error) {
     // Request not successful
-    res.status(409).json({ error: err.message });
+    res.status(409).json({ error: error.message });
     console.log("Error:", error.message);
   }
 };
 
-const deleteTask = async (req, res) => {
+const deleteGoal = async (req, res) => {
   try {
     const id = parseInt(req.params.id); //  parse the ID from the URL parameter and convert it to an integer.
     // (IMPORTANT: THE PARAMETER MUST MATCH THE ONE USED IN THE routes file)
 
-    // Query to delete the selected task
-    const query = `DELETE FROM tasks WHERE id = $1`;
+    // Query to delete the selected goal
+    const query = `DELETE FROM goals WHERE id = $1`;
 
     const results = await pool.query(query, [id]);
 
     // HTTP DELETE request
-    res.status(200).json(results.rows); // Request successful, send message to client
+    res.status(200).json(results.rows[0]); // Request successful, send message to client
   } catch (error) {
     // Request not successful
-    res.status(409).json({ error: err.message });
+    res.status(409).json({ error: error.message });
     console.log("Error:", error.message);
   }
 };
 
 module.exports = {
-  createTask,
-  getTaskById,
-  getTasksByUserId,
-  updateTask,
-  deleteTask,
+  createGoal,
+  getGoalById,
+  getGoalsByUserId,
+  updateGoal,
+  deleteGoal,
 };
 /*
 export default {
-  createTask,
-  getTaskById,
-  getTasksByUserId,
-  updateTask,
-  deleteTask,
+  createGoal,
+  getGoalById,
+  getGoalsByUserId,
+  updateGoal,
+  deleteGoal,
 };
 */
---
+++ backend/src/routes/goals.routes.js
@@ -0,0 +1,25 @@
+// Created by: Jorge Valdes-Santiago
+//
+//
+// This script contains the endpoints to the goals controller functions
+const express = require("express"); //import express from "express";
+const GoalsController = require("../controllers/goals.controller.js"); //import TasksController from "../controllers/tasks.controller.js";
+
+const router = express.Router();
+
+// HTTP GET /api/goals/
+router.get("/", GoalsController.getGoalsByUserId);
+
+// HTTP POST /api/goals
+router.post("/", GoalsController.createGoal);
+
+// HTTP GET /api/goals/:id
+router.get("/:id", GoalsController.getGoalById);
+
+// HTTP PATCH /api/goals/:id
+router.patch("/:id", GoalsController.updateGoal);
+
+// HTTP DELETE /api/goals/:id
+router.delete("/:id", GoalsController.deleteGoal);
+
+module.exports = router; //export default router;
