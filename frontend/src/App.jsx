6169c8267f564cf1cee3b6112e6d2b7a90986709
JV-FIU <jvald.fiu@hotmail.com>
2025-11-18 23:47:44 -0500

    Updated App.jsx
    Added a list of routes to pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,55 @@
 // frontend/src/App.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useRoutes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [goals, setGoals] = useState([]);
+  const [focusSessions, setFocusSessions] = useState([]);
+  const [habits, setHabits] = useState([]);
+  const [user, setUser] = useState([]);
+
+  // Load tasks
+  //useEffect()
+
+  // Routes and pages
+
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/tasks/create",
+      element: "<Task Creator page here>",
+    },
+    {
+      path: "/goals",
+      element: "<Goals page here>",
+    },
+    {
+      path: "/goals/create/",
+      element: "<Goals Creation page here>",
+    },
+    {
+      path: "/habits",
+      element: "<Habits page here>",
+    },
+    {
+      path: "/habits/create/",
+      element: "<Habit Creator page here>",
+    },
+    {
+      path: "/insights",
+      element: "<Insights page here>",
+    },
+    {
+      path: "/settings",
+      element: "<Settings page here>",
+    },
+  ]);
+
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
