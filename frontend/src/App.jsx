a49fbc79e774b69a022c647513e03ef7166a745a
JV-FIU <jvald.fiu@hotmail.com>
2025-11-19 00:15:19 -0500

    Updated App.jsx
    Changed the route style.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 // frontend/src/App.jsx
 import { Routes, Route, useRoutes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import { useEffect, useState } from "react";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -10,8 +11,34 @@ function App() {
   const [habits, setHabits] = useState([]);
   const [user, setUser] = useState([]);
 
-  // Load tasks
-  //useEffect()
+  const API_URL = "http://localhost:3001"; // link to API
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetch(`${API_URL}/auth/login/success`, {
+        credentials: "include",
+      });
+      const json = await response.json();
+      setUser(json.user);
+    };
+
+    const fetchTasks = async () => {};
+
+    const fetchGoals = async () => {};
+
+    const fetchHabits = async () => {};
+
+    // Call functions
+    //getUser();
+  });
+
+  const logout = async () => {
+    // Make a request to the /auth/logout endpoint with the credentials option set to 'include'.
+    const url = `${API_URL}/auth/logout`;
+    const response = await fetch(url, { credentials: "include" });
+    const json = await response.json(); // Convert response to JSON
+    window.location.href = "/"; // Redirect to home page
+  };
 
   // Routes and pages
 
@@ -49,6 +76,10 @@ function App() {
       element: "<Settings page here>",
     },
   ]);
+
+  return <>{element}</>;
+
+  // Delete or comment the "return" above if it causes problems
 
   return (
     <Routes>
