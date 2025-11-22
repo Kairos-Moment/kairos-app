320745930b6e04d9df02c448df516875c64e09e2
JNet46 <bowersjudah22@gmail.com>
2025-11-21 20:11:04 -0500

    Typo on one of the methods

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: 'https://kairos-app.onrender.com', 
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
